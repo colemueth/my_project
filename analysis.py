ced8880f242270736c0d02d6c0dd4f32ecf7f129
Cole Mueth <nicholas.mueth@wsu.edu>
2018-03-01 10:16:40 -0800

    added .py file

---
+++ analysis.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 09:56:47 2018
+
+@author: home
+"""
+
+import pandas as pd
+dat = pd.read_csv('data/gapminder_gdp_europe.csv')
